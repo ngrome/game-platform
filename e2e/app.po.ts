7537716cc2276a8a413bae1b3a147936b50abef2
Luciano.M <luciano.murruni@gmail.com>
2018-03-10 18:13:18 +0100

    Init app with angular-cli

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('gplay-root h1')).getText();
+  }
+}
