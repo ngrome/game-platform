d85f9bc887a56a11ffc62695ac451f5ebbb8055f
Luciano.M <luciano.murruni@gmail.com>
2018-03-18 20:37:43 +0100

    Add template of signin and recovery password

--- src/app/auth/auth.routing.module.ts
+++ src/app/auth/auth.routing.module.ts
@@ -1,3 +1,5 @@
+import { RecoveryComponent } from './recovery/recovery.component';
+import { AuthComponent } from './auth.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +7,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const authRoutes: Routes = [
-  { path: 'auth', component: LoginComponent }
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'signin', component: RegistrationComponent },
+      { path: 'recovery', component: RecoveryComponent },
+    ]
+  },
 ];
 
 @NgModule({
